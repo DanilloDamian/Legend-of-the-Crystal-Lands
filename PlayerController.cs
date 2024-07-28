f64ffbeb5ebaeee1e02bf95a433ab347a93fff22
Danillo <danillo.damian@hotmail.com>
2024-07-28 11:06:01 -0300

    Adicionado controles basicos do Player

---
+++ PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    [Header("Config Player")]
+    public float movementSpeed = 3f;
+
+    private Vector3 direction;
+
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude > 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+        }
+
+        characterController.Move(direction * movementSpeed * Time.deltaTime);
+
+
+    }
+}
